fb29ba39287fad0ee7ac3770fecf2b8c10b80fd3
SunnyGJing <806416908@qq.com>
2019-05-18 17:16:39 +0800

    Update 2.两数相加.cpp

--- LeetCode/2.两数相加.cpp
+++ LeetCode/2.两数相加.cpp
@@ -11,9 +11,11 @@ return: ListNode*: 2个非空链表分别按位存储了一个整数，用同样
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+    
+    
 class Solution_1 {
-// 简单地新建结点并插入到单链表中
-// O(n)+O(n)
+    // 简单地新建结点并插入到单链表中
+    // O(n)+O(n)
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         // 计算加和数值、进位数值
@@ -53,6 +55,26 @@ public:
         if(remain != 0) {
             ListNode* pNode = new ListNode(remain);
             p->next = pNode;
+        };
+        return head;
+    }
+};
+
+
+class toLearn_2 {
+    // 用递归实现重复的插入和值节点的操作
+    // O(n)+O(n)
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL and l2 == NULL) return NULL;
+        else if (l1 == NULL) return l2;
+        else if (l2 == NULL) return l1;
+
+        int adds = l1->val + l2->val;
+        ListNode* head = new ListNode(adds % 10);
+        head->next = addTwoNumbers(l1->next, l2->next);
+        if(adds>9) {
+            head->next = addTwoNumbers(head->next, new ListNode(1));
         };
         return head;
     }
